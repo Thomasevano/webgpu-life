8da32d546a457c9cebd895a3af202fa98632c309
Thomasevano <thomas-evano@hotmail.fr>
2025-06-21 18:37:02 +0200

    feat: draw geometry

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,67 @@ context.configure({
   format: canvasFormat,
 });
 
+const vertices = new Float32Array([
+  //   X,    Y,
+  -0.8, -0.8, // Triangle 1 (Blue)
+  0.8, -0.8,
+  0.8, 0.8,
+
+  -0.8, -0.8, // Triangle 2 (Red)
+  0.8, 0.8,
+  -0.8, 0.8,
+]);
+
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [{
+    format: "float32x2",
+    offset: 0,
+    shaderLocation: 0, // Position, see vertex shader
+  }],
+};
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: `
+    @vertex
+    fn vertexMain(@location(0) pos: vec2<f32>) -> 
+    @builtin(position) vec4<f32> {
+      return vec4f(pos, 0, 1); // (x, y, z, w)
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4<f32> {
+      return vec4f(1, 0, 0, 1); // (r, g, b, a)
+    }
+  `
+});
+
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout]
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [{
+      format: canvasFormat
+    }]
+  }
+});
+
+
+
 const encoder = device.createCommandEncoder();
 
 const pass = encoder.beginRenderPass({
@@ -27,6 +88,9 @@ const pass = encoder.beginRenderPass({
     storeOp: "store",
   }]
 });
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2);
 pass.end();
 
 const commandBuffer = encoder.finish();
