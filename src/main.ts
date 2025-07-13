38085b99816c647eb7e1b3470241e0b57c13f613
Thomasevano <thomas-evano@hotmail.fr>
2025-07-13 13:23:01 +0200

    chore(webgpu): draw a grid

--- src/main.ts
+++ src/main.ts
@@ -46,16 +46,26 @@ const vertexBufferLayout = {
 const cellShaderModule = device.createShaderModule({
   label: "Cell shader",
   code: `
-    @vertex
-    fn vertexMain(@location(0) pos: vec2<f32>) -> 
-    @builtin(position) vec4<f32> {
-      return vec4f(pos, 0, 1); // (x, y, z, w)
-    }
+    @group(0) @binding(0) var<uniform> grid: vec2f;
 
-    @fragment
-    fn fragmentMain() -> @location(0) vec4<f32> {
-      return vec4f(1, 0, 0, 1); // (r, g, b, a)
-    }
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f, 
+                  @builtin(instance_index) instance: u32) -> 
+      @builtin(position) vec4f {
+
+        let i = f32(instance);
+        let cell = vec2f(i % grid.x,floor(i / grid.x));
+
+        let cellOffset = cell / grid * 2;
+        let gridPos = (pos + 1) / grid - 1 + cellOffset;
+
+        return vec4f(gridPos, 0, 1); // (x, y, z, w)
+      }
+
+      @fragment
+      fn fragmentMain() -> @location(0) vec4<f32> {
+        return vec4f(1, 0, 0, 1); // (r, g, b, a)
+      }
   `
 });
 
@@ -76,7 +86,25 @@ const cellPipeline = device.createRenderPipeline({
   }
 });
 
+const GRID_SIZE = 32;
+const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+const uniformBuffer = device.createBuffer({
+  label: "Uniform buffer",
+  size: uniformArray.byteLength,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(uniformBuffer, /*bufferOffset=*/0, uniformArray);
 
+const bindGroup = device.createBindGroup({
+  label: "Cell renderer bind group",
+  layout: cellPipeline.getBindGroupLayout(0),
+  entries: [{
+    binding: 0,
+    resource: {
+      buffer: uniformBuffer,
+    },
+  }],
+});
 
 const encoder = device.createCommandEncoder();
 
@@ -90,7 +118,10 @@ const pass = encoder.beginRenderPass({
 });
 pass.setPipeline(cellPipeline);
 pass.setVertexBuffer(0, vertexBuffer);
-pass.draw(vertices.length / 2);
+
+pass.setBindGroup(0, bindGroup);
+
+pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
 pass.end();
 
 const commandBuffer = encoder.finish();
