f45c253297542638fd7dd2f7dc4a18d616c0333a
Thomasevano <thomas-evano@hotmail.fr>
2025-07-30 12:32:36 +0200

    chore: run the simulation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+const GRID_SIZE = 64;
+const UPDATE_INTERVAL = 100; // Update every 200ms (5 times/sec)
+const WORKGROUP_SIZE = 16;
+
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 
 if (!navigator.gpu) {
@@ -16,10 +20,6 @@ context.configure({
   device: device,
   format: canvasFormat,
 });
-
-const GRID_SIZE = 32;
-const UPDATE_INTERVAL = 200; // Update every 200ms (5 times/sec)
-let step = 0; // Track how many simulation steps have been run
 
 
 const vertices = new Float32Array([
@@ -49,42 +49,33 @@ const vertexBufferLayout = {
   }],
 };
 
-const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
-const uniformBuffer = device.createBuffer({
-  label: "Uniform buffer",
-  size: uniformArray.byteLength,
-  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+const bindGroupLayout = device.createBindGroupLayout({
+  label: "Cell Bind Group Layout",
+  entries: [{
+    binding: 0,
+    visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE,
+    buffer: {}
+  },
+  {
+    binding: 1,
+    visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE,
+    buffer: {
+      type: "read-only-storage"
+    }
+  },
+  {
+    binding: 2,
+    visibility: GPUShaderStage.COMPUTE,
+    buffer: {
+      type: "storage"
+    }
+  }]
 });
-device.queue.writeBuffer(uniformBuffer, /*bufferOffset=*/0, uniformArray);
 
-// Create an array representing the active state of each cell.
-const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE);
-
-// Create a storage buffer to hold the cell state.
-const cellStateStorage = [
-  device.createBuffer({
-    label: "Cell State A",
-    size: cellStateArray.byteLength,
-    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-  }),
-  device.createBuffer({
-    label: "Cell State B",
-    size: cellStateArray.byteLength,
-    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-  })
-];
-
-// Mark every third cell of the first grid as active.
-for (let i = 0; i < cellStateArray.length; i += 3) {
-  cellStateArray[i] = 1;
-}
-device.queue.writeBuffer(cellStateStorage[0], 0, cellStateArray);
-
-// Mark every other cell of the second grid as active.
-for (let i = 0; i < cellStateArray.length; i++) {
-  cellStateArray[i] = i % 2;
-}
-device.queue.writeBuffer(cellStateStorage[1], 0, cellStateArray);
+const pipelineLayout = device.createPipelineLayout({
+  label: "Cell Pipeline Layout",
+  bindGroupLayouts: [bindGroupLayout]
+});
 
 const cellShaderModule = device.createShaderModule({
   label: "Cell shader",
@@ -106,32 +97,27 @@ const cellShaderModule = device.createShaderModule({
     fn vertexMain(input: VertexInput) -> VertexOutput {
         let i = f32(input.instance);
         let cell = vec2f(i % grid.x,floor(i / grid.x));
-        let state = f32(cellState[input.instance]);
 
+        let scale = f32(cellState[input.instance]);
         let cellOffset = cell / grid * 2;
-        let gridPos = (input.pos*state + 1) / grid - 1 + cellOffset;
+        let gridPos = (input.pos*scale + 1) / grid - 1 + cellOffset;
 
         var output: VertexOutput;
         output.pos = vec4f(gridPos, 0, 1); // (x, y, z, w)
-        output.cell = cell;
+        output.cell = cell / grid;
         return output;
     }
 
-    // struct FragInput {
-    //   @location(0) cell: vec2f,
-    // };
-
     @fragment
     fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
-    let c = input.cell / grid;
-      return vec4f(c, 1-c.x, 1); // (r, g, b, a)
+      return vec4f(input.cell, 1.0 - input.cell.x, 1); // (r, g, b, a)
     }
   `
 });
 
 const cellPipeline = device.createRenderPipeline({
   label: "Cell pipeline",
-  layout: "auto",
+  layout: pipelineLayout,
   vertex: {
     module: cellShaderModule,
     entryPoint: "vertexMain",
@@ -146,10 +132,108 @@ const cellPipeline = device.createRenderPipeline({
   }
 });
 
+const simulationShaderModule = device.createShaderModule({
+  label: "Game of Life simulation shader",
+  code: `
+    @group(0) @binding(0) var<uniform> grid: vec2f;
+
+    @group(0) @binding(1) var<storage> cellStateIn: array<u32>;
+    @group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
+
+    fn cellIndex(cell: vec2u) -> u32 {
+      return (cell.y % u32(grid.y)) * u32(grid.x) +
+             (cell.x % u32(grid.x));
+    }
+
+    fn cellActive(x: u32, y: u32) -> u32 {
+      return cellStateIn[cellIndex(vec2(x, y))];
+    }
+
+    @compute @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE})
+    fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
+      if (cellStateIn[cellIndex(cell.xy)] == 1) {
+        cellStateOut[cellIndex(cell.xy)] = 0;
+      } else {
+        cellStateOut[cellIndex(cell.xy)] = 1;
+      }
+
+      let activeNeighbors = cellActive(cell.x - 1, cell.y - 1) +
+                            cellActive(cell.x - 1, cell.y) +
+                            cellActive(cell.x - 1, cell.y + 1) +
+                            cellActive(cell.x, cell.y - 1) +
+                            cellActive(cell.x, cell.y + 1) +
+                            cellActive(cell.x + 1, cell.y - 1) +
+                            cellActive(cell.x + 1, cell.y) +
+                            cellActive(cell.x + 1, cell.y + 1);
+
+      let i = cellIndex(cell.xy);
+
+      // Conway's game of life rules:
+      switch activeNeighbors {
+        case 2: { // Active cells with 2 neighbors stay active.
+          cellStateOut[i] = cellStateIn[i];
+        }
+        case 3: { // Cells with 3 neighbors become or stay active.
+          cellStateOut[i] = 1;
+        }
+        default: { // Cells with < 2 or > 3 neighbors become inactive.
+          cellStateOut[i] = 0;
+        }
+      }
+
+    }`
+});
+
+const simulationPipeline = device.createComputePipeline({
+  label: "Simulation pipeline",
+  layout: pipelineLayout,
+  compute: {
+    module: simulationShaderModule,
+    entryPoint: "computeMain"
+  }
+});
+
+const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+const uniformBuffer = device.createBuffer({
+  label: "Uniform buffer",
+  size: uniformArray.byteLength,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(uniformBuffer, /*bufferOffset=*/0, uniformArray);
+
+// Create an array representing the active state of each cell.
+// Set each cell to a random state, then copy the JavaScript array 
+// into the storage buffer.
+const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE);
+for (let i = 0; i < cellStateArray.length; i++) {
+  cellStateArray[i] = Math.random() < 0.6 ? 1 : 0;
+}
+
+// Create a storage buffer to hold the cell state.
+const cellStateStorage = [
+  device.createBuffer({
+    label: "Cell State A",
+    size: cellStateArray.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  }),
+  device.createBuffer({
+    label: "Cell State B",
+    size: cellStateArray.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  })
+];
+device.queue.writeBuffer(cellStateStorage[0], 0, cellStateArray);
+
+// Mark every other cell of the second grid as active.
+for (let i = 0; i < cellStateArray.length; i++) {
+  cellStateArray[i] = i % 2;
+}
+device.queue.writeBuffer(cellStateStorage[1], 0, cellStateArray);
+
 const bindGroups = [
   device.createBindGroup({
     label: "Cell renderer bind group A",
-    layout: cellPipeline.getBindGroupLayout(0),
+    layout: bindGroupLayout,
     entries: [{
       binding: 0,
       resource: {
@@ -161,11 +245,17 @@ const bindGroups = [
       resource: {
         buffer: cellStateStorage[0],
       },
+    },
+    {
+      binding: 2,
+      resource: {
+        buffer: cellStateStorage[1],
+      },
     }],
   }),
   device.createBindGroup({
     label: "Cell renderer bind group B",
-    layout: cellPipeline.getBindGroupLayout(0),
+    layout: bindGroupLayout,
     entries: [{
       binding: 0,
       resource: {
@@ -177,15 +267,33 @@ const bindGroups = [
       resource: {
         buffer: cellStateStorage[1],
       },
+    },
+    {
+      binding: 2,
+      resource: {
+        buffer: cellStateStorage[0],
+      },
     }],
   })
 ];
 
+let step = 0; // Track how many simulation steps have been run
 function updateGrid() {
+  const encoder = device.createCommandEncoder();
+
+  const computePass = encoder.beginComputePass();
+
+  computePass.setPipeline(simulationPipeline);
+  computePass.setBindGroup(0, bindGroups[step % 2]);
+
+  const workGroupCount = Math.ceil(GRID_SIZE / WORKGROUP_SIZE);
+  computePass.dispatchWorkgroups(workGroupCount, workGroupCount);
+
+  computePass.end();
+
   step++;
 
   // start a render pass
-  const encoder = device.createCommandEncoder();
   const pass = encoder.beginRenderPass({
     colorAttachments: [{
       view: context.getCurrentTexture().createView(),
